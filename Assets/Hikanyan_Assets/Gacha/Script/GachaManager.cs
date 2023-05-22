c57a026490f13d93a9925c12fc34efb2f1c314ce
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-05-22 13:39:13 +0900

    add

---
+++ Assets/Hikanyan_Assets/Gacha/Script/GachaManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hikanyan.Runner
+{
+    public class GachaManager : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+}
