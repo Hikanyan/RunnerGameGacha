0392e30cae37b9df84f4f4a42dcb011139ec1bfd
Hikanyan <hikanyan0131@gmail.com>
2023-06-05 06:42:59 +0900

    [Add]Playerのステートパターン

---
+++ Assets/Hikanyan_Assets/Gacha/Script/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hikanyan.Runner.Player
+{
+    public partial class Player :MonoBehaviour
+    {
+        private PlayerStateBase _currentState;
+        void Start()
+        {
+            _currentState.OnEnter(this,null);
+        }
+
+        void Update()
+        {
+            _currentState.OnUpdate(this);
+        }
+
+        void ChangeState(PlayerStateBase nextState)
+        {
+            _currentState.OnExit(this,nextState);
+            nextState.OnExit(this,_currentState);
+            _currentState = nextState;
+        }
+        void OnCollisionEnter(Collision collision)
+        {
+            //ChangeState(stateStanding);
+        }
+    }
+}
---
+++ Assets/Hikanyan_Assets/Gacha/Script/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+namespace Hikanyan.Runner.Player
+{
+    public class PlayerController:MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+        }
+    }
+}
